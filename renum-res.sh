0582e5798210c01c7ba2e118be0214455909a303
Clement Viricel <cviricel@tapou.toulouse.inra.fr>
2016-06-24 14:24:52 +0200

    NEW VERSION OF RENUM: CHAIN CHANGE

--- renum-res.sh
+++ renum-res.sh
@@ -2,7 +2,14 @@
 source /home/dsimoncini/softs/ccp4-6.4.0/setup-scripts/ccp4.setup-sh
 
 pdbset xyzin $1 xyzout $2 1>/dev/null <<EOF
-renumber 1
+renumber 1 
 exclude HOH 
 exclude HEADER 
+EOF
+
+pdbcur xyzin $2 xyzout $2 1>/dev/null <<EOF
+renchain /*/A 'A'
+renchain /*/B 'A'
+renchain /*/C 'B'
+renchain /*/D 'B'
 EOF
